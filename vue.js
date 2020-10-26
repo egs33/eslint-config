b8fbf4665bbe9a054545bb2fef6db32c577800c6
egs33 <egs33.bicycle@gmail.com>
2020-10-26 21:18:28 +0900

    Add rules for vue

--- vue.js
+++ vue.js
@@ -2,4 +2,14 @@ module.exports = {
   extends: ['./browser', 'plugin:vue/recommended'],
   plugins: ['vue'],
   // parser: 'vue-eslint-parser',
+  rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 4,
+    }],
+    'vue/html-indent': ['error', 2],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'never',
+    }],
+  },
 };
