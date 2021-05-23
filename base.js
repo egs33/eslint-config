d403daf009f72e2055386ab8489440deb9cae20c
egs33 <egs33.bicycle@gmail.com>
2021-05-23 21:16:50 +0900

    Update rule

--- base.js
+++ base.js
@@ -9,10 +9,12 @@ module.exports = {
   rules: {
     'no-plusplus': 'off',
     'no-continue': 'off',
-    'func-style': ['error', 'expression'],
+    'func-style': 'error',
     'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
     'max-lines': ['error', { max: 500, skipComments: true }],
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js#L6
+    'array-bracket-newline': 'error',
   },
 };
